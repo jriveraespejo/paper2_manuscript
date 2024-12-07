78a7af13d0e0dafcfa172ef193c8b2b8c8c1d7bf
jriveraespejo <jriveraespejo@gmail.com>
2024-12-07 16:48:38 +0100

    section:issues,CaseV; process:write

--- code/figures.R
+++ code/figures.R
@@ -81,27 +81,23 @@ axis( side=1, at=c(0, dd[1]), tick=F,
       label=c(0, expression( S[B] - S[A] ) ) )
 
 # figure parameters
-pplotD = c( dnorm( dd[1], mean=dd[1], sd=dd[2] ),
+pplotD = c( dnorm( 0, mean=dd[1], sd=dd[2] ),
+            dnorm( dd[1], mean=dd[1], sd=dd[2] ),
+            dnorm( dd[1]+dd[2], mean=dd[1], sd=dd[2]),
             dnorm( dd[1], mean=dd[1], sd=dd[2] ) + 0.05,
-            dnorm( dd[1] + dd[2], mean=dd[1], sd=dd[2]),
-            dnorm( dd[1]+0.4, mean=dd[1], sd=dd[2] )-0.14,
-            dnorm( 0, mean=dd[1], sd=dd[2] ) )
+            dnorm( dd[1]+0.4, mean=dd[1], sd=dd[2] )-0.14 )
 
 # discriminal difference
-colorArea( from=0, to=7, density=dnorm, mean=dd[1], sd=dd[2],col=rgb(0,0,0,0.05) )
+colorArea( from=0, to=7, density=dnorm, mean=dd[1], sd=dd[2], col=rgb(0,0,0,0.05) )
 curve( dnorm(x, mean=dd[1], sd=dd[2]), lwd=1.5, add=T ) 
-# text( x=dd[1], y=pplotD[2], cex=0.8, 'Text B' ) 
-lines( x=c(dd[1], dd[1]), y=c(pplotD[1], -0.1), lty=2, lwd=0.7 )
+lines( x=c(0,0), y=c(pplotD[1],-0.1), lty=2, lwd=0.7 )
+text( x=0.45, y=0.03, cex=0.8, expression( P(B>A) ) ) 
+lines( x=c(dd[1], dd[1]), y=c(pplotD[2], -0.1), lty=2, lwd=0.7 )
 lines( x=c(dd[1], dd[1]+dd[2]), y=rep( pplotD[3], 2), lty=2, lwd=0.7 )
-text( x=dd[1]+0.5, y=pplotD[4], cex=0.8, expression( sigma[AB] ) ) 
-# text( x=dd[1]+1.65, y=0.32, cex=0.8, 'where:' )
-# text( x=dd[1]+2, y=pplotD[4], cex=0.8,
-#       expression( S[BA] == S[B] - S[A]) )
-# text( x=dd[1]+2.45, y=pplotD[4]-0.04, cex=0.8,
-#       expression( sigma[BA] == sqrt( sigma[B]^2 + sigma[A]^2 - rho*sigma[B]*sigma[A]) ) )
-lines( x=c(0,0), y=c(pplotD[5],-0.1), lty=2, lwd=0.7 )
-# text( x=-0.5, y=0.05, cex=0.8, expression( P(B<A) ) ) 
-text( x=0.5, y=0.05, cex=0.8, expression( P(B>A) ) ) 
+text( x=dd[1]+0.6, y=pplotD[3]-0.02, cex=0.8, expression( sigma[AB] ) ) 
+text( x=dd[1]+1.7, y=0.25, cex=0.8, 'where:' )
+text( x=dd[[1]]+2.4, y=0.22, cex=0.8,
+      expression( sigma[BA] == sqrt( sigma[B]^2 + sigma[A]^2 - rho*sigma[B]*sigma[A]) ) )
 
 dev.off()
 
@@ -168,22 +164,27 @@ plot( NULL, xlim=c(-2,5), ylim=c(0,0.6),
 axis( side=1, labels=F, lwd.ticks=0 )
 axis( side=1, at=0, tick=F, label='0' )
 
-# distributions
+# figure parameters
 l_ty = c( 1, rep(2,2), rep(3,2) )
 l_wd = c( 1.5, seq(1,0.5,length.out=4) )
-text = paste0( ' ~ sigma[B] - sigma[A] == ', dpB_d - dpA[2] )
+text = paste0( ' ~ sigma[B] - sigma[A] == ', dpB_d - dpA )
+pplotD = c( dnorm( 0, mean=dd[[1]], sd=dd[[2]][1] ),
+            dnorm( dd[[1]], mean=dd[[1]], sd=dd[[2]][1] ),
+            dnorm( dd[[1]] + 2*dd[[2]][1], mean=dd[[1]], sd=dd[[2]][1] ),
+            dnorm( dd[[1]]+0.4, mean=dd[[1]], sd=dd[[2]][1] )-0.14 )
+
+
+# distributions
 colorArea( from=0, to=7, density=dnorm, mean=dd[[1]], sd=dd[[2]][1], col=rgb(0,0,0,0.05) )
 for(i in 1:length(dd[[2]])){
   # colorArea( from=0, to=7, density=dnorm, mean=dd[[1]], sd=dd[[2]][i], col=rgb(0,0,0,0.05) )
   curve( dnorm(x, mean=dd[[1]], sd=dd[[2]][i]), lwd=l_wd[i], lty=l_ty[i], add=T )
 }
-lines( x=c(0,0), y=c(0.21,-0.1), lty=2, lwd=0.7 )
-text( x=0.5, y=0.05, cex=0.8, expression( P(B>A) ) ) 
+lines( x=c(0,0), y=c(pplotD[1],-0.1), lty=2, lwd=0.7 )
+text( x=0.45, y=0.03, cex=0.8, expression( P(B>A) ) ) 
 legend('topleft', lwd=l_wd, lty=l_ty, bty='n', legend=parse(text=text) )
 
 dev.off()
-
-
 
 
 # figure 5 ####
